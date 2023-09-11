31d7c19ae4fbd84401f9714c23f2d5bc768aa2ce
Tyson Nguyen <yngu4822@gmail.com>
2023-09-11 17:56:23 +1000

    update loops in python :book:

--- sesh5-loops.py
+++ sesh5-loops.py
@@ -33,5 +33,24 @@
 #     print(i, '\t', i*i)
 
 # c. Print 100 down to 1
-for i in range(100, 0, -1):
-    print(i)
+# for i in range(100, 0, -1):
+#     print(i)
+
+# d. get 5 inputs, print total of the 5 inputs
+# total = 0
+# for i in range(1, 5):
+#     current_input = int(input(f"Insert a number that will be summed {i}.:"))
+#     total += current_input
+#     print(f"your input: {current_input},current total: {total}")
+# print(total)
+
+# EX3: test score, get user input and check if num exist in range 1 - 100
+# user_input = int(input("Insert your test mark:"))
+# status = False
+# for i in range(0, 101):
+#     if i == user_input:
+#         status = True
+# if status:
+#     print(f"Your result:{user_input}")
+# else:
+#     print("Invalid result number, please provide it as a number(0 - 100)")
